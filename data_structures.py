bb493431cf53d77ed8acc4f6d7f4784b179a7a8c
Saeed Bash <saeedbash979@gmail.com>
2026-01-07 09:54:41 +0300

    Create queue class.

--- data_structures.py
+++ data_structures.py
@@ -1,5 +1,7 @@
 # To Implement some Data Structures from scratch; To deepen understanding and practice programming
 
+from collections import deque
+
 class Stack:
 
     def __init__(self):
@@ -30,8 +32,8 @@ class Stack:
         return self.stack[-1]
     
     def pop(self):
-        temp = self.stack[-1]
-        self.stack = self.stack[:-1]
+        temp = self.stack[0]
+        self.stack = self.stack[1:]
         return temp
     
     def pop_all(self):
@@ -46,8 +48,60 @@ class Stack:
     def size(self):
         return len(self.stack)
 
+class Queue:
 
-#Stack Tests
+    def __init__(self):
+        self.queue = deque()
+
+    def __repr__(self):
+
+        if self.isEmpty() == True:
+            return "Empty queue"
+
+        string = "[End of queue]     "
+        for i in range(self.size()):
+            string = string + str(self.queue[i]) + "     " 
+        string = string + "[Start of queue]"
+
+        return string
+
+    def push(self, item):
+        self.queue.appendleft(item)
+
+    def push_multi(self, iterable):
+
+        for item in iterable:
+            self.push(item)
+            
+        
+    def peek(self, i=1):
+        
+        if self.isEmpty() == True:
+            return "queue is empty"
+        
+        try:
+            return self.queue[-i]
+        except IndexError:
+            return f"Last position in queue is #{self.size()}"
+            
+    
+    def pop(self):
+        return self.queue.pop()
+    
+    def pop_all(self):
+        temp = self.queue
+        self.queue = []
+        return temp
+
+    def isEmpty(self):
+        if self.size() == 0: return True
+        return False
+    
+    def size(self):
+        return len(self.queue)
+    
+
+# Stack Tests:
 
     # new = Stack()
 
@@ -62,10 +116,42 @@ class Stack:
     # print(new.peek())
     # print(new.pop())
     # print(new.size())
+    
     # list = ["Potato", "Carrot", "Tomato"]
     # new.push_multi(list)
     # print(new.stack)
     # print(new)
+    
     # new.pop_all()
     # print(new)
 
+# Queue Tests:
+
+    # new = Queue()
+
+    # print(new.isEmpty())
+    # print(new)
+
+    # new.push("Potato")
+    # print(new.isEmpty())
+    # print(new.size())
+    # print(new)
+
+    # print(new.peek())
+    # print(new.peek(1))
+    # print(new.peek(2))
+    # print(new.pop())
+    # print(new.size())
+
+    # list = ["Potato", "Carrot", "Tomato"]
+    # new.push_multi(list)
+    # print(new.queue)
+    # print(new)
+    # print(new.pop())
+    # new.push("Banana")
+    # print(new)
+    # print(new.isEmpty())
+    # new.pop_all()
+    # print(new.isEmpty())
+    # print(new.size())
+    # print(new)
