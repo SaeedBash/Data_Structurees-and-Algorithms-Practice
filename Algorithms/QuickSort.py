facead9346b7696b1369c4c10ee1d2b819473c33
Saeed Bash <saeedbash979@gmail.com>
2026-01-14 12:20:03 +0300

    Implement Lomuto partition scheme for quick sort method.

--- Algorithms/QuickSort.py
+++ Algorithms/QuickSort.py
@@ -44,18 +44,45 @@ def clear_screen():
 
 #     return left, right
 
-# VERY CLEAN VERSION
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
+# VERY CLEAN VERSION, NAIVE 
+# def quick_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
 
-    pivot = arr[randint(0, len(arr)-1)]
+#     pivot = arr[randint(0, len(arr)-1)]
 
-    left  = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
+#     left  = [x for x in arr if x < pivot]
+#     middle = [x for x in arr if x == pivot]
+#     right = [x for x in arr if x > pivot]
 
-    return quick_sort(left) + middle + quick_sort(right)
+#     return quick_sort(left) + middle + quick_sort(right)
+
+# Lomuto Partition Version:
+def quick_sort(arr, low=0, high=None):
+    if high is None:
+        high = len(arr) - 1
+
+    if low < high:
+        pivot_index = randint(low, high)
+        arr[pivot_index], arr[high] = arr[high], arr[pivot_index]
+        
+        pi = partition(arr, low, high)
+
+        quick_sort(arr, low, pi - 1)
+        quick_sort(arr, pi + 1, high)
+
+def partition(arr, low, high):
+    pivot = arr[high]  # pivot is at the end
+    i = low - 1        # index of smaller element
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    # Place pivot in the correct position
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
 
 clear_screen()
 
